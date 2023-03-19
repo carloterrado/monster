7caec1f0531b80cd00335cd1fd1837a3ff14c7cd
carloterrado <iamterradocarlo@gmail.com>
2023-03-19 17:50:44 +0800

    done class component react course

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Component } from "react";
 
-import logo from "./logo.svg";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
-    console.log("constructor");
+    document.title = "Monsters";
     this.state = {
       monsters: [],
       searchValue: '',
@@ -15,7 +17,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log("componentDidmount");
+    
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -27,35 +29,34 @@ class App extends Component {
       );
   }
 
-  render() {
-    console.log("render");
+  onChangeValue = (event) => {
+    const searchValue = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchValue };
+    });
+  }
 
-    const filteredMonster = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchValue);
+  render() {
+   
+    const { monsters, searchValue } = this.state;
+    const { onChangeValue } = this;
+
+    const filteredMonster = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchValue);
     })
     return (
-      <div className="App bg-slate-600 h-screen w-screen px-2 py-10 sm:p-14">
+      <div className="App bg-slate-600 min-h-screen min-w-screen px-2 py-10 sm:p-14">
+
+       
 
         {/* SEARCH INPUT FOR MONSTER */}
-        <div className=" flex justify-end">
-          <input className="border px-4 py-2 w-full max-w-md focus:outline-none rounded-lg " type="search" placeholder="search monster..." 
-            onChange={ (event) => {
-              const searchValue = event.target.value.toLocaleLowerCase();
-              this.setState(() => {
-                return { searchValue };
-              });
-            }
-          } />
+        <div className=" flex justify-between flex-wrap gap-4">
+          <h1 className="text-xl md:text-4xl font-bold text-white text-center md:text-start w-full md:w-1/2">List of Monster</h1>
+          <SearchBox onChangeHandler={ onChangeValue } placeholder="search pokemon..." className="border px-4 py-2 w-full max-w-md mx-auto md:mx-0 focus:outline-none rounded-lg" />
         </div>
 
-        {/* DISPLAY ALL MONSTER */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 mt-10 gap-4">
-          {
-            filteredMonster.map((monster) => {
-              return <h1 className="border text-white" key={monster.id}>{monster.name}</h1>;
-            })
-          }
-        </div>
+        {/* DISPLAY ALL MONSTER */} 
+        <CardList monsters={ filteredMonster } />
       </div>
     );
   }
